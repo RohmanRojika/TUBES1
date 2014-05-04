ba7a75a3fffa595becfa2135c1fcf4746c86673e
RohmanRojika <rohman.rojika@student.unsil.ac.id>
2014-05-04 09:32:40 -0700

    a
    a

--- oke zone3.php
+++ oke zone3.php
@@ -1,1 +1,21 @@
-a
+<?php
+// to retrieve selected html data, try these DomXPath examples:
+
+$file ="http://techno.okezone.com/breaking/56";
+$doc = new DOMDocument();
+$doc->loadHTMLFile($file);
+
+$xpath = new DOMXpath($doc);
+$elements = $xpath->query("*/div[@id='subkanal-content-list fl']");
+
+if (!is_null($elements)) {
+  foreach ($elements as $element) {
+    echo "<br/>[". $element->nodeName. "]";
+
+    $nodes = $element->childNodes;
+    foreach ($nodes as $node) {
+      echo $node->nodeValue. "\n";
+    }
+  }
+}
+?>
