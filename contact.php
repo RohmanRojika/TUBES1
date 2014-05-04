e42ab354335879e93d5a2918d98e79fe0beca6b8
RohmanRojika <rohman.rojika@student.unsil.ac.id>
2014-05-04 09:31:57 -0700

    a
    a

--- contact.php
+++ contact.php
@@ -1,1 +1,44 @@
-c
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Tugas Layanan Web</title>
+ <link href="web.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="isi">
+ <div id="atas">
+  <div id="logo">
+  <img src="img/logo.png" alt="logo">
+  </div>
+  <div id="nav">
+  <ul class="nav-link">
+     <li id="acitve-nav"><a href="index.php">Home</a></li>
+     <li><a href="tugas.php.php">Tugas</a></li>
+     <li><a href="about.php.php">About</a></li>
+     <li><a href="contact.php.php">Contact</a></li>
+  </ul>
+  </div>
+ </div>
+ <div id="konten">
+ <table border="0" width="800" height="500">
+ <tr>
+ <td align="center">
+ <h1>Contact</h1>
+ <h3>Email: </h3>
+ <h3>Hp:</h3>
+ </td>
+ </tr>
+ </table>
+ </div>
+ <div id="footer">
+  <div id="footer-link">
+  <a href="index.php">Home</a> | <a href="tugas.php">Tugas</a> | <a href="about.php">About</a> | <a href="contact.php">Contact</a>
+  </div>
+  <div id="footer-copy">
+  <p>&copy; Tugas Layanan Web E 2011</p>
+  </div>
+ </div>
+</div>
+
+</body>
+</html>
